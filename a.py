5b8a9851d5efae1849e8d6824ac4391b0fc1933b
Riddhi_Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2022-10-18 21:06:12 +0400

    Update a.py

--- a.py
+++ a.py
@@ -1,5 +1,6 @@
 myset = {"hello", " there", "is", "this", "mutable",False,0}
 print(myset)
 
-
+myset[0] = "bye" #'set' object does not support item assignment
+del myset[0] # set' object doesn't support item deletion
 #main
